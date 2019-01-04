beb6819d232050476acaeecb6e6e88de8dc364ce
adamguarino <aguarin3@binghamton.edu>
2019-01-04 16:50:19 -0500

    printed Hello World

---
+++ GitTest.java
@@ -0,0 +1,11 @@
+package GitTest;
+
+public class GitTest {
+	private int numCounter;
+	public GitTest(int num) {
+		numCounter = num;
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
